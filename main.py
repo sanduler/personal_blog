26a60de13939b0c6f09edca6306b1dd5fb4f30d5
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-20 16:34:05 -0700

    created the basic html/css/js/py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+# Name: Ruben Sanduleac
+# Date: March 20th, 2022
+# Description: This is a personal blog built using Flask, BootStrap, HTML, CSS, JS, and Python.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = Flask(__name__)
