128b3b040b15fd5d85f5bca190273c908edaf11b
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-20 19:28:51 -0700

    navigation is fully functional

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index.html')
 def index_page():
     return render_template('index.html')
 
-@app.route('/about')
+
+@app.route('/about.html')
 def about_page():
     return render_template('about.html')
 
-@app.route('/contact')
+
+@app.route('/contact.html')
 def contact_page():
     return render_template('contact.html')
 
+@app.route('/post.html')
+def post_page():
+    return render_template('post.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
