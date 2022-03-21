37c49e79210db23fc87e47f71ad5402fb155f4c9
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-20 19:11:30 -0700

    corrected the images for the webpages

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 def index_page():
     return render_template('index.html')
 
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
